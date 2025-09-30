ff0de4e59c5a0b26ee1e7477b5055af7ec039614
Chris Bayer <christopher.bayer@luminartech.com>
2025-09-30 14:21:55 +0200

    fixed bug

--- main.py
+++ main.py
@@ -22,19 +22,30 @@ class UserStatsTracker:
     def add_user(self, user_id, status=UserStatus.UNKNOWN):
         while self.is_locked:
             time.sleep(0.1)
+        self.is_locked = True
         self.user_stats[user_id] = {"status": status, "last_seen": time.time()}
+        self.is_locked = False
 
     def get_user_stats(self):
-        return self.user_stats
+        while self.is_locked:
+            time.sleep(0.1)
+        self.is_locked = True
+        user_stats = self.user_stats.copy()
+        self.is_locked = False
+        return user_stats
 
     def clean_up_old_users(self):
         if self.is_locked:
             return
 
         self.is_locked = True
+        users_to_delete = []
         for user_id, user_data in self.user_stats.items():
             if time.time() - user_data["last_seen"] > 4:
-                del self.user_stats[user_id]
+                users_to_delete.append(user_id)
+        
+        for user_id in users_to_delete:
+            del self.user_stats[user_id]
         self.is_locked = False
 
 
@@ -49,6 +60,7 @@ def main():
 
     if "user_id" not in st.session_state:
         st.session_state.user_id = str(uuid.uuid4())
+        
 
     user_stats_tracker = get_user_stats_tracker()
     user_stats_tracker.add_user(st.session_state.user_id)
