958d2f4a2b0c2c12bb875883bd89c73ff959ad46
Chris Bayer <christopher.bayer@luminartech.com>
2025-10-02 13:56:04 +0200

    BUG

--- main.py
+++ main.py
@@ -20,9 +20,13 @@ class UserStatsTracker:
         self._user_stats = {}
         self._lock = threading.RLock()
 
-    def add_user(self, user_id, status=UserStatus.UNKNOWN):
+    def set_user_status(self, user_id, status=UserStatus.UNKNOWN):
         with self._lock:
             self._user_stats[user_id] = {"status": status, "last_seen": time.time()}
+
+    def set_user_active(self, user_id):
+        with self._lock:
+            self._user_stats[user_id]["last_seen"] = time.time()
 
     def get_user_stats(self):
         with self._lock:
@@ -48,19 +52,62 @@ def get_user_stats_tracker():
 def main():
     st_autorefresh(interval=2000, key="data_refresh")
 
+    user_stats_tracker = get_user_stats_tracker()
     if "user_id" not in st.session_state:
         st.session_state.user_id = str(uuid.uuid4())
+        user_stats_tracker.set_user_status(st.session_state.user_id, UserStatus.UNKNOWN)
 
-    user_stats_tracker = get_user_stats_tracker()
-    user_stats_tracker.add_user(st.session_state.user_id)
+    user_stats_tracker.set_user_active(st.session_state.user_id)
 
     user_stats_tracker.clean_up_old_users()
 
     st.title("Lecture Feedback App")
     st.write(f"Num Users: {len(user_stats_tracker.get_user_stats())}")
 
+    # create red green yellow buttons
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        if st.button("🔴 Red"):
+            user_stats_tracker.set_user_status(st.session_state.user_id, UserStatus.RED)
+
+    with col2:
+        if st.button("🟡 Yellow"):
+            user_stats_tracker.set_user_status(
+                st.session_state.user_id, UserStatus.YELLOW
+            )
+
+    with col3:
+        if st.button("🟢 Green"):
+            user_stats_tracker.set_user_status(
+                st.session_state.user_id, UserStatus.GREEN
+            )
+
+    # show accumulated color stats
+    st.title("Accumulated Color Stats")
+
+    user_stats = user_stats_tracker.get_user_stats()
+    st.write(f"User stats: {user_stats}")
+    st.write(f"User stats: {user_stats.values()}")
+    red_count = len(
+        [user for user in user_stats.values() if user["status"] == UserStatus.RED]
+    )
+    yellow_count = sum(
+        1 for user in user_stats.values() if user["status"] == UserStatus.YELLOW
+    )
+    green_count = sum(
+        1 for user in user_stats.values() if user["status"] == UserStatus.GREEN
+    )
+
+    st.write(f"Red: {red_count}")
+    st.write(f"Yellow: {yellow_count}")
+    st.write(f"Green: {green_count}")
+
     st.title("Debug Output:")
-    st.write(f"current User ID: {st.session_state.user_id}")
+    st.write(
+        f"current User ID: {st.session_state.user_id}",
+        f"Status: {user_stats_tracker.get_user_stats()[st.session_state.user_id]['status']}",
+    )
     st.write(f"Current active users: {len(user_stats_tracker.get_user_stats())}")
     for user_id, status in user_stats_tracker.get_user_stats().items():
         st.write(f"- active user ID: {user_id}, Status: {status}")
