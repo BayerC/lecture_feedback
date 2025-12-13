8e03796d4de2739c6b0925b6f6f427bed567e06b
Chris Bayer <c4.bayer@gmail.com>
2025-12-13 17:17:42 +0100

    improve test

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,6 +7,30 @@ def run_wrapper() -> None:
     run()
 
 
+def is_on_room_selection_screen(app: AppTest) -> bool:
+    return (
+        len(app.title) == 1 and app.title[0].value == "Welcome to Lecture Feedback App"
+    )
+
+
+def is_on_active_room_screen(app: AppTest) -> bool:
+    return len(app.title) == 1 and app.title[0].value == "Active Room"
+
+
 def test_app_initial_load() -> None:
     app = AppTest.from_function(run_wrapper)
     app.run()
+
+    assert is_on_room_selection_screen(app)
+
+    app.button(key="join_room").click().run()
+    assert is_on_room_selection_screen(app)
+
+    app.text_input(key="join_room_id").set_value("1337").run()
+    app.button(key="join_room").click().run()
+    assert len(app.error) == 1
+    assert app.error[0].value == "Room ID not found"
+    assert is_on_room_selection_screen(app)
+
+    app.button(key="start_room").click().run()
+    assert is_on_active_room_screen(app)
