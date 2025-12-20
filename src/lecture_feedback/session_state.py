766f2620f0a34e864ef90904632d81e337c53a96
Chris Bachhuber <cbachhuber89@gmail.com>
2025-12-20 20:52:10 +0100

    Remove leftover member

--- src/lecture_feedback/session_state.py
+++ src/lecture_feedback/session_state.py
@@ -3,8 +3,6 @@ from typing import cast
 
 import streamlit as st
 
-from lecture_feedback.user_stats_tracker import UserStatus
-
 
 class SessionState:
     """Per-user session state wrapper.
@@ -16,12 +14,10 @@ class SessionState:
     def __init__(self) -> None:
         if "session_id" not in st.session_state:
             st.session_state.session_id = str(uuid.uuid4())
-        self.user_status = UserStatus.UNKNOWN
 
     @property
     def session_id(self) -> str:
         return cast("str", st.session_state.session_id)
-
 
 
 # Userstate should be stored in the session state
