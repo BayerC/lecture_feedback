b009453f71099e7e32720d43ad87b47b3adb7daf
Chris Bachhuber <cbachhuber89@gmail.com>
2025-12-25 20:05:57 +0100

    wip: two sessions in same room

--- tests/test_app.py
+++ tests/test_app.py
@@ -53,6 +53,10 @@ def test_join_existing_room() -> None:
     assert is_on_active_room_screen(app)
 
 
+def get_page_content(app: AppTest) -> str:
+    return "\n".join(element.value for element in app.markdown)
+
+
 def test_click_buttons_in_new_room() -> None:
     app = AppTest.from_function(run_wrapper)
     app.run()
@@ -65,5 +69,42 @@ def test_click_buttons_in_new_room() -> None:
         UserStatus.GREEN,
     ):
         app.button(key=status.value).click().run()
-        page_content = "\n".join(element.value for element in app.markdown)
-        assert status.value in page_content
+        assert status.value in get_page_content(app)
+
+
+def test_two_sessions_track_independent_user_stats() -> None:
+    app1 = AppTest.from_function(run_wrapper)
+    app1.run()
+    app1.button(key="start_room").click().run()
+
+    room_id = None
+    for element in app1.markdown:
+        if element.value.startswith("**Room ID:**"):
+            room_id = element.value.split("`")[1]
+            break
+    assert room_id is not None
+
+    app2 = AppTest.from_function(run_wrapper)
+    app2.run()
+    app2.text_input(key="join_room_id").set_value(room_id).run()
+    app2.button(key="join_room").click().run()
+
+    app1.button(key=UserStatus.RED.value).click().run()
+    page_content1 = get_page_content(app1)
+    assert UserStatus.RED.value in page_content1
+    assert UserStatus.UNKNOWN.value in page_content1
+
+    # page_content2 = get_page_content(app2)
+    # assert UserStatus.RED.value in page_content2
+    # assert UserStatus.UNKNOWN.value in page_content2
+
+    # app2.button(key=UserStatus.GREEN.value).click().run()
+    # page_content1 = get_page_content(app1)
+    # assert UserStatus.RED.value in page_content1
+    # assert UserStatus.GREEN.value in page_content1
+    # assert UserStatus.UNKNOWN.value not in page_content1
+
+    # page_content2 = get_page_content(app2)
+    # assert UserStatus.RED.value in page_content2
+    # assert UserStatus.GREEN.value in page_content2
+    # assert UserStatus.UNKNOWN.value not in page_content2
