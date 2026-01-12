3f62ff5e507c9fa9c37f79f920f0851c395c6f36
Chris Bayer <c4.bayer@gmail.com>
2026-01-12 06:50:29 +0100

    refactored

--- src/lecture_feedback/app.py
+++ src/lecture_feedback/app.py
@@ -46,33 +46,23 @@ def show_active_room(room: RoomState) -> None:
 
     current_user_status = room.get_user_status()
 
+    status_options = [
+        UserStatus.GREEN,
+        UserStatus.YELLOW,
+        UserStatus.RED,
+    ]
+    status_captions = [
+        "Following easily",
+        "Need more explanation",
+        "Cannot follow",
+    ]
+
     if current_user_status == UserStatus.UNKNOWN:
-        status_options = [
-            UserStatus.UNKNOWN,
-            UserStatus.GREEN,
-            UserStatus.YELLOW,
-            UserStatus.RED,
-        ]
-        status_captions = [
-            "Not decided yet",
-            "can follow easily",
-            "needs more explanation",
-            "cannot follow at all",
-        ]
-    else:
-        status_options = [
-            UserStatus.GREEN,
-            UserStatus.YELLOW,
-            UserStatus.RED,
-        ]
-        status_captions = [
-            "can follow easily",
-            "needs more explanation",
-            "cannot follow at all",
-        ]
+        status_options.insert(0, UserStatus.UNKNOWN)
+        status_captions.insert(0, "Not decided yet")
 
     selected_user_status = st.radio(
-        "How good can you follow the lecture?",
+        "How well can you follow the lecture?",
         status_options,
         format_func=lambda s: s.value,
         captions=status_captions,
@@ -80,10 +70,11 @@ def show_active_room(room: RoomState) -> None:
 
     room.set_user_status(selected_user_status)
 
-    if selected_user_status != UserStatus.UNKNOWN and current_user_status == UserStatus.UNKNOWN:
+    if (
+        selected_user_status != UserStatus.UNKNOWN
+        and current_user_status == UserStatus.UNKNOWN
+    ):
         st.rerun()
-
-
 
     for sid, user_status in room.get_room_participants():
         st.write(f"Session {sid}: {user_status.value}")
